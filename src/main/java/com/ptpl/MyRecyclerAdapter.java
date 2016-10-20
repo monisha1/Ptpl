7b85afa476eb7271b9fbc757e0b0ca6bfe96d16b
monisha1 <monisha.divi@gmail.com>
2016-10-20 20:05:55 +0530

    Listitem with vertical ellipse

--- src/main/java/com/ptpl/MyRecyclerAdapter.java
+++ src/main/java/com/ptpl/MyRecyclerAdapter.java
@@ -3,16 +3,27 @@ package com.ptpl;
 /**
  * Created by MC11 on 5/23/2016.
  */
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
 import com.squareup.picasso.Picasso;
 import java.util.List;
 
@@ -35,8 +46,8 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
     }
 
     @Override
-    public void onBindViewHolder(CustomViewHolder customViewHolder, int i) {
-        FeedItem feedItem = feedItemList.get(i);
+    public void onBindViewHolder(final CustomViewHolder customViewHolder, int i) {
+        final FeedItem feedItem = feedItemList.get(i);
 
         //Download image using picasso library
         Picasso.with(mContext).load(feedItem.getThumbnail())
@@ -52,8 +63,85 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
 
         customViewHolder.textView.setTag(customViewHolder);
         customViewHolder.imageView.setTag(customViewHolder);
+        customViewHolder.buttonViewOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //will show popup menu here
+                //creating a popup menu
+                PopupMenu popup = new PopupMenu(mContext, customViewHolder.buttonViewOption);
+                //inflating menu from xml resource
+                popup.inflate(R.menu.options_menu);
+                //adding click listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu1:
+                                //handle menu1 click
+                              /* Bitmap image = BitmapFactory.decodeResource(mContext.getResources(), Integer.parseInt(feedItem.getThumbnail()));
+                                SharePhoto photo = new SharePhoto.Builder()
+                                        .setBitmap(image)
+                                        .setCaption(feedItem.getTitle())
+                                        .build();
+                                SharePhotoContent content = new SharePhotoContent.Builder()
+                                        .addPhoto(photo)
+                                        .build();
+                                 ShareButton shareButton = (ShareButton) view.findViewById(R.id.fb_share_button);
+                                shareButton.setShareContent(content);*/
+                                //try {
+                                    Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+                                    shareIntent.setType("text/html");
+                                    shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Test Mail");
+                                    shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Launcher");
+                                    shareIntent.putExtra(Intent.EXTRA_STREAM, feedItem.getThumbnail());
+                                    shareIntent.putExtra(Intent.EXTRA_TEXT, feedItem.getTitle());
+                                    shareIntent.setPackage("com.facebook.katana");
+                                    mContext.startActivity(Intent.createChooser(shareIntent,"via"));
+                               /* PackageManager packManager = mContext.getPackageManager();
+                                List<ResolveInfo> resolvedInfoList = packManager.queryIntentActivities(shareIntent,  PackageManager.MATCH_DEFAULT_ONLY);
+
+                                boolean resolved = false;
+                                for(ResolveInfo resolveInfo: resolvedInfoList){
+                                    if(resolveInfo.activityInfo.packageName.contains("com.facebook.katana")) {
+                                        shareIntent.setClassName(
+                                                resolveInfo.activityInfo.packageName,
+                                                resolveInfo.activityInfo.name );
+                                        resolved = true;
+                                        break;
+                                    }
+                                }
+                                if(resolved){
+                                    mContext.startActivity(shareIntent);
+                                }else{
+                                    Toast.makeText(mContext, "Facebook App is not installed", Toast.LENGTH_LONG).show();
+                                }
+                        } catch (final ActivityNotFoundException e) {
+                            Toast.makeText(mContext, "You don't seem to have Facebook App installed on this device", Toast.LENGTH_SHORT).show();
+                        }
+                             //  mContext.startActivity(shareIntent);//Intent.createChooser(shareIntent, "Share Deal"));
+*/
+                                break;
+
+                            case R.id.menu2:
+                                //handle menu2 click
+                                break;
+                            case R.id.menu3:
+                                //handle menu3 click
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                //displaying the popup
+                popup.show();
+
+            }
+        });
 
     }
+
+
 
     @Override
     public int getItemCount() {
@@ -78,11 +166,19 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
     public class CustomViewHolder extends RecyclerView.ViewHolder {
         protected ImageView imageView;
         protected TextView textView;
+        public TextView buttonViewOption;
 
         public CustomViewHolder(View view) {
             super(view);
             this.imageView = (ImageView) view.findViewById(R.id.thumbnail);
             this.textView = (TextView) view.findViewById(R.id.title);
+            buttonViewOption = (TextView) itemView.findViewById(R.id.textViewOptions);
         }
     }
-}
+
+
+    private void sendShareFacebook() {
+
+    }
+    }
+
--- src/main/java/com/ptpl/RecyclerListFrag.java
+++ src/main/java/com/ptpl/RecyclerListFrag.java
@@ -1,6 +1,9 @@
 package com.ptpl;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,6 +14,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,11 +40,26 @@ import java.util.List;
  * Created by MC11 on 5/23/2016.
  */
 public class RecyclerListFrag extends Fragment{
-    private static final String TAG = "RecyclerViewExample";
+    //private static final String TAG = "RecyclerViewExample";
     private List<FeedItem> feedsList;
     private RecyclerView mRecyclerView;
     private MyRecyclerAdapter adapter;
     private ProgressBar progressBar;
+
+    private static String TAG = RecyclerListFrag.class.getName();
+    private CallbackManager callbackManager;
+    ShareDialog shareDialog;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize facebook SDK.
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        // Create a callbackManager to handle the login responses.
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+        // this part is optional
+        //shareDialog.registerCallback(callbackManager, callback);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,6 +80,46 @@ public class RecyclerListFrag extends Fragment{
 
 
     }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+      //  setImageShare(view);
+    }
+   /* private void setImageShare(View view) {
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.ani_cat);
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(image)
+                .setCaption("#Tutorialwing")
+                .build();
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+        ShareButton shareButton = (ShareButton) view.findViewById(R.id.fb_share_button);
+        shareButton.setShareContent(content);
+    }*/
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Call callbackManager.onActivityResult to pass login result to the LoginManager via callbackManager.
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    private FacebookCallback<Sharer.Result> callback = new FacebookCallback<Sharer.Result>() {
+        @Override
+        public void onSuccess(Sharer.Result result) {
+            Log.v(TAG, "Successfully posted");
+            // Write some code to do some operations when you shared content successfully.
+        }
+        @Override
+        public void onCancel() {
+            Log.v(TAG, "Sharing cancelled");
+            // Write some code to do some operations when you cancel sharing content.
+        }
+        @Override
+        public void onError(FacebookException error) {
+            Log.v(TAG, error.getMessage());
+            // Write some code to do some operations when some error occurs while sharing content.
+        }
+    };
 
 public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
 
